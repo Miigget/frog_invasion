aca6ae6aa12b1fc5c064425407fc0496ae8fa954
Miigget <marcinsdl@icloud.com>
2024-12-19 19:35:29 +0000

    create settings.py with class for settings

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -1,14 +1,15 @@
 import sys
 import pygame
 
+from settings import Settings
+
 def run_game():
-    """Initialize game and create a screen object"""
+    """Initialize game, settings and create a screen object"""
 
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    fi_settings = Settings()
+    screen = pygame.display.set_mode((fi_settings.screen_width, fi_settings.screen_height))
     pygame.display.set_caption("Frog Invasion")
-
-    bg_color = (100, 100, 220)
 
     # start the main loop for the game
     while True:
@@ -18,7 +19,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         
-        screen.fill(bg_color)
+        screen.fill(fi_settings.bg_color)
 
         # make the most recently drawn screen visible
         pygame.display.flip()
---
+++ src/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all game settings"""
+
+    def __init__(self):
+        """Initialize game settings"""
+        # screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (100, 100, 220)
