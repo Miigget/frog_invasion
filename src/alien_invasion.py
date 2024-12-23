abd43b17f47ababd7ed8f7b87a5911a78d3947f3
migget <marcinsdl@icloud.com>
2024-12-23 13:51:14 +0100

    add movement to the ship

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -17,8 +17,10 @@ def run_game():
     # start the main loop for the game
     while True:
 
-        gf.check_events()
+        gf.check_events(ship)
+
+        ship.update()
         
-        gf.update_screen()
+        gf.update_screen(fi_settings, screen, ship)
 
 run_game()
