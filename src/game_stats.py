152d2ecf180c1ba09f6512a25e70f9ca27d54122
Miigget <marcinsdl@icloud.com>
2025-01-04 19:17:06 +0000

    create GameStats class

---
+++ src/game_stats.py
@@ -0,0 +1,11 @@
+class GameStats():
+    """Track statistics for the game"""
+
+    def __init__(self, fi_settings):
+        """Initialize statistics"""
+        self.fi_settings = fi_settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game"""
+        self.ships_left = self.fi_settings.ship_limit
