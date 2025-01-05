fc66bee8d68aa50a29e3283b75fbd489c447ffac
Miigget <marcinsdl@icloud.com>
2025-01-05 15:46:14 +0000

    create button class

--- src/game_stats.py
+++ src/game_stats.py
@@ -6,7 +6,7 @@ class GameStats():
         self.fi_settings = fi_settings
         self.reset_stats()
         # start game in an active state
-        self.game_active = True
+        self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
